4e650c7ef35625f424ef2a5fa14ed97ac80855a8
AlinaF <afassahova9@gmail.com>
2022-11-11 00:22:25 +0500

    add test

--- src/main/java/ru/netology/interfase/Main.java
+++
@@ -1,7 +0,0 @@
-package ru.netology.interfase;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ src/main/java/ru/netology/interfase/TicketRepositiry.java
@@ -0,0 +1,2 @@
+package ru.netology.interfase;public class TicketRepositiry {
+}
